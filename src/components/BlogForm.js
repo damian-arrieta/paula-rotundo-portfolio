251473ed7b55e10dcdd4670e0d41f8611b27a056
arrdamian <arrdamian@icloud.com>
2023-03-26 16:06:54 -0300

    Add styles in form

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -27,40 +27,47 @@ const BlogForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className='form-container'>
+      <h3>Escribe una nueva publicación al Blog:</h3>
       {entries.map((entry, index) => (
-        <div key={index}>
+        <div key={index} className="entry">
           <select
             value={entry.type}
             onChange={(event) => {
               const newEntries = [...entries];
               newEntries[index].type = event.target.value;
               setEntries(newEntries);
-            }}
-          >
-            <option value="h1">H1</option>
-            <option value="h2">H2</option>
-            <option value="h3">H3</option>
-            <option value="h4">H4</option>
-            <option value="h5">H5</option>
-            <option value="h6">H6</option>
-            <option value="p">Párrafo</option>
-            <option value="imagen">Imagen</option>
-          </select>
-          <input
-            type="text"
-            value={entry.text}
-            onChange={(event) => handleEntryChange(index, event)}
-          />
-          <button type="button" onClick={() => handleRemoveEntry(index)}>
-            Eliminar
-          </button>
-        </div>
-      ))}
-      <button type="button" onClick={handleAddEntry}>
-        Agregar entrada
-      </button>
-      <button type="submit">Subir al blog</button>
+              }}
+            >
+              <option value="h1">H1</option>
+              <option value="h2">H2</option>
+              <option value="h3">H3</option>
+              <option value="h4">H4</option>
+              <option value="h5">H5</option>
+              <option value="h6">H6</option>
+              <option value="p">Párrafo</option>
+              <option value="img-blog">Imagen de Blog</option>
+              <option value="img-card">Imagen de Card</option>
+            </select>
+            <input
+              className='form-control'
+              type="text"
+              value={entry.text}
+              onChange={(event) => handleEntryChange(index, event)}
+            />
+            <button type="button" onClick={() => handleRemoveEntry(index)}>
+              <i className="bi bi-x-circle"></i>
+            </button>
+            <button type="button" onClick={handleAddEntry} className="add-entry">
+              <i className="bi bi-plus-circle"></i>
+            </button>
+          </div>
+        ))}
+          <div className="button-group">
+            <button type="submit" className="submit">
+              Subir al blog
+            </button>
+          </div>
     </form>
   );
 };
