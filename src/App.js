2b07793912717aa23b7e59461e233f323827fd75
arrdamian <arrdamian@icloud.com>
2023-03-27 18:17:24 -0300

    Import useState in App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
+
   return (
     <>
       <BrowserRouter>
