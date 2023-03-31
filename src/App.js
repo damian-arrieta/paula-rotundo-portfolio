53ed2a8fac9b3775542e1e20edf6d1b4fe577e85
arrdamian <arrdamian@icloud.com>
2023-03-31 11:51:39 -0300

    Reset

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Footer from './components/Footer';
-import BlogDetail from './components/BlogDetail';
 import './App.css';
 
 function App() {
-  const [selectedBlog, setSelectedBlog] = useState(null);
-
-  const handleBlogSelect = (blog) => {
-    setSelectedBlog(blog);
-  }
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home handleBlogSelect={handleBlogSelect} />} />
-          <Route path='/posteos/:posteoId' element={<BlogDetail selectedBlog={selectedBlog} />} />
+          <Route path='/' element={<Home />} />
         </Routes>
       </BrowserRouter>
       <Footer />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import Card from './Card';
-import BlogForm from './BlogForm';
-import { Link } from 'react-router-dom';
-import { getBlogData } from '../utils/api';
+import React from 'react';
 import Header from './Header';
 
 const Home = () => {
-  const [selectedBlog, setSelectedBlog] = useState(null);
-  const [blogs, setBlogs] = useState([]);
-
-  useEffect(() => {
-    getBlogData().then((data) => {
-      setBlogs(data);
-    }).catch((error) => {
-      console.log(error);
-    });
-  }, []);
-
-  const handleBlogSelect = (blog) => {
-    setSelectedBlog(blog);
-  }
-
   return (
     <div className='home-container'>
       <Header />
-      <BlogForm handleBlogSelect={handleBlogSelect} />
-      <div className='cards-container'>
-        {blogs.map((blog, index) => (
-          <Link key={blog.id} to={`/posteos/${blog.id}`}>
-            <Card entryData={blog} />
-          </Link>
-        ))}
-      </div>
     </div>
   );
 }
