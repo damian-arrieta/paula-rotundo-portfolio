84d416f488d90c254e281d514f42c45f05ab13c5
arrdamian <arrdamian@icloud.com>
2023-04-02 18:41:03 -0300

    Add logic in PostDetailContainer

--- src/components/PostDetailContainer.js
+++ src/components/PostDetailContainer.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import PostDetail from './PostDetail';
+
+const post = [
+    {id: 1, image: 'https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png', title: 'Primer Post', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+]
 
 export default function PostDetailContainer() {
+
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        const getData = new Promise(resolve => {
+            resolve(post);
+        });
+        getData.then(res => setData(res));
+    }, []);
+
   return (
-    <div>PostDetailContainer</div>
+    <PostDetail data={data} />
   )
 }
