b460b37337d18bcbeba4a1b7dd2372660d989a55
arrdamian <arrdamian@icloud.com>
2023-04-09 13:12:49 -0300

    Add styles in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,29 +3,29 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between p-3 mb-4 border-bottom">
-        <div className="col-md-3 mb-2 mb-md-0">
+    <nav className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between p-3 mb-4 line-bottom">
+      <div className="col-md-3 mb-2 mb-md-0">
         <Link to={"/"} className="d-inline-flex link-body-emphasis text-decoration-none">
-          <h2>Paula Rotundo</h2>
+          <h2 className='text-principal'>Paula Rotundo</h2>
         </Link>
       </div>
 
       <div className="col-md-3 text-end">
-      <a className='btn btn-outline-primary me-2' href="https://sites.google.com/view/ganeshinbound/inicio">
-        <i className="bi bi-book"></i>
-      </a>
-      <a className='btn btn-outline-primary me-2' href="">
-        <i className="bi bi-linkedin"></i>
-      </a>
-      <a className='btn btn-outline-primary me-2' href="">
-        <i className="bi bi-instagram"></i>
-      </a>
-      <a className='btn btn-outline-primary me-2' href="">
-        <i className="bi bi-envelope-paper-heart"></i>
-      </a>
-      <a className='btn btn-outline-primary me-2' href="">
-        <i className="bi bi-whatsapp"></i>
-      </a>
+        <a className='btn btn-navbar me-2' href="https://sites.google.com/view/ganeshinbound/inicio">
+          <i className="bi bi-book"></i>
+        </a>
+        <a className='btn btn-navbar me-2' href="">
+          <i className="bi bi-linkedin"></i>
+        </a>
+        <a className='btn btn-navbar me-2' href="">
+          <i className="bi bi-instagram"></i>
+        </a>
+        <a className='btn btn-navbar me-2' href="">
+          <i className="bi bi-envelope-paper-heart"></i>
+        </a>
+        <a className='btn btn-navbar me-2' href="">
+          <i className="bi bi-whatsapp"></i>
+        </a>
       </div>
     </nav>
   )
