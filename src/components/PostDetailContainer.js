c9e4f2b6f9bba6074cc24d921bdb1ba5624623b9
arrdamian <arrdamian@icloud.com>
2023-04-11 17:22:39 -0300

    Install Axios

--- src/components/PostDetailContainer.js
+++ src/components/PostDetailContainer.js
@@ -3,12 +3,6 @@ import { useParams } from 'react-router';
 import PostDetail from './PostDetail';
 import Navbar from './Navbar';
 
-const posts = [
-    { id: 1, image: 'https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png', title: 'Primer Post', content: '...' },
-    { id: 2, image: 'https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png', title: 'Segundo Post', content: '...' },
-    { id: 3, image: 'https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png', title: 'Tercer Post', content: '...' }
-];
-
 export default function PostDetailContainer() {
     const [data, setData] = useState({});
 
