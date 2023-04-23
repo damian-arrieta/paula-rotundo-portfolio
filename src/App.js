81674d3fa3acf78a677547c46dfefabf7af0f406
arrdamian <arrdamian@icloud.com>
2023-04-23 12:12:27 -0300

    Delete components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Views/Home';
-import PostDetailContainer from './components/PostDetailContainer';
+import Header from './components/Header'
 import Footer from './components/Footer';
-import BlogEntryForm from './Views/BlogEntryForm';
-import Login from './Views/Login';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
-  const [user, setUser] = useState(null);
-
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/posts/:id' element={<PostDetailContainer />} />
-          {user ? (
-            <Route path='/form' element={<BlogEntryForm user={user} />} />
-          ) : (
-            <Route
-            path='/login'
-            element={<Login setUser={setUser} /> } />
-          )}
-        </Routes>
-      </BrowserRouter>
+      <Header />
       <Footer />
 </>
 );
